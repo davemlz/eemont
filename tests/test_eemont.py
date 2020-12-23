68a6d8e982c9f159391064f47d2c41ef8e10fbc6
U-dmlmont-PC\dmlmont <dml.mont@gmail.com>
2020-12-23 00:03:36 -0500

    Tests updated, tutorials created

--- tests/test_eemont.py
+++ tests/test_eemont.py
@@ -1,6 +1,6 @@
 import ee
 import unittest
-from eemont import Sentinel2
+from eemont import Sentinel2, visualization
 
 ee.Initialize()
 
@@ -32,6 +32,26 @@ class Test(unittest.TestCase):
     def test_Sentinel2_spectralIndex_02(self):
         """Test the cloud mask function returns an Image"""        
         self.assertIsInstance(Sentinel2.spectralIndex(S2.first()), ee.image.Image)
+        
+    def test_visualization_rgb(self):
+        """Test the visualization function returns a dictionary"""        
+        self.assertIsInstance(visualization.rgb(), dict)
+        
+    def test_visualization_infrared(self):
+        """Test the visualization function returns a dictionary"""        
+        self.assertIsInstance(visualization.infrared(), dict)
+        
+    def test_visualization_landwater(self):
+        """Test the visualization function returns a dictionary"""        
+        self.assertIsInstance(visualization.landwater(), dict)
+        
+    def test_visualization_vegetation(self):
+        """Test the visualization function returns a dictionary"""        
+        self.assertIsInstance(visualization.vegetation(), dict)
+        
+    def test_visualization_index(self):
+        """Test the visualization function returns a dictionary"""        
+        self.assertIsInstance(visualization.index(), dict)
         
 if __name__ == '__main__':
     unittest.main()

39f69f8ab0af16e294b3ae8fb320ef866214cfbb
U-dmlmont-PC\dmlmont <dml.mont@gmail.com>
2021-04-05 16:40:18 -0500

    Docs updated

--- docs/conf.py
+++ docs/conf.py
@@ -51,12 +51,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'sphinx_rtd_theme'
 #html_theme = "insegel"
 #html_theme = "divio_docs_theme"

a12e04b68d1f0051ddab09a103f9f1561167e2ba
U-dmlmont-PC\dmlmont <dml.mont@gmail.com>
2021-04-28 16:48:53 -0500

    Tests updated

---
+++ .github/scripts/update_ee_token.py
@@ -0,0 +1,9 @@
+import os
+# Token from SECRETS
+credentials = '{"refresh_token":"%s"}' % os.environ["EE_TOKEN"]
+# We need this location to save the token
+credentialsPath = os.path.expanduser("~/.config/earthengine/")
+os.makedirs(credentialsPath, exist_ok = True)
+# Write the token
+with open(credentialsPath + "credentials", "w") as f:
+    f.write(credentials)

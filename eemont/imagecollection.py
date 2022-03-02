20ec79ed2f7327db2d00ae48264733c3f0d1b3a1
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-03-02 22:26:42 +0100

    DOCS: tabs instead of code-tabs (#59)

--- eemont/imagecollection.py
+++ eemont/imagecollection.py
@@ -126,6 +126,35 @@ def closest(self, date, tolerance=1, unit="month"):
             Initialize()
             eemont = pyimport("eemont");
             S2 = EE.ImageCollection('COPERNICUS/S2_SR') |> x -> closest(x,'2020-10-15');
+
+    .. tabs::
+
+        .. tab:: Python
+
+            .. code-block:: python
+
+                import ee, eemont
+                ee.Initialize()
+                S2 = ee.ImageCollection('COPERNICUS/S2_SR').closest('2020-10-15')
+
+        .. tab:: R
+
+            .. code-block:: r
+
+                library(rgee)
+                library(reticulate)
+                ee_Initialize()
+                eemont <- import("eemont")
+                S2 <- ee$ImageCollection('COPERNICUS/S2_SR')$closest('2020-10-15')
+
+        .. tab:: Julia
+
+            .. code-block:: julia
+
+                using EarthEngine, PyCall
+                Initialize()
+                eemont = pyimport("eemont");
+                S2 = EE.ImageCollection('COPERNICUS/S2_SR') |> x -> closest(x,'2020-10-15');
     """
     return ee_extra.ImageCollection.core.closest(self, date, tolerance, unit)
 

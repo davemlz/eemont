050f8c0c4af91f69e4c806c7da88666727385f06
davemlz <dml.mont@gmail.com>
2022-03-05 21:37:49 +0100

    FEAT: Pinned ee_extra 0.0.11

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "beautifulsoup4",
         "earthengine-api",
-        "ee_extra>=0.0.9",
+        "ee_extra>=0.0.11",
         "numpy",
         "pandas",
         "python-box",

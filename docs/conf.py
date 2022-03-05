bdb389730e38ae562503e64974cd602444c7acd4
davemlz <dml.mont@gmail.com>
2022-03-05 21:38:43 +0100

    VERSION: 0.3.2

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, David Montero Loaiza"
 author = "David Montero Loaiza"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.1"
+release = "0.3.2"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(filename):
 
 setup(
     name="eemont",
-    version="0.3.1",
+    version="0.3.2",
     url="https://github.com/davemlz/eemont",
     license="MIT",
     author="David Montero Loaiza",

63660fb9420d59e35a2d26ef094ed1c8a7802514
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2022-04-04 14:43:50 +0200

    FEAT: Pinned ee-extra 0.0.13

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     install_requires=[
         "beautifulsoup4",
         "earthengine-api",
-        "ee_extra>=0.0.12",
+        "ee_extra>=0.0.13",
         "numpy",
         "pandas",
         "python-box",

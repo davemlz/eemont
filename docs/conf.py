1dcbc9f3c6ef565dd172583b86201a51ffc1d77c
David Montero Loaiza <49817852+davemlz@users.noreply.github.com>
2023-01-23 10:13:45 +0100

    VERSION: 0.3.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, David Montero Loaiza"
 author = "David Montero Loaiza"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.5"
+release = "0.3.6"
 
 # -- General configuration ---------------------------------------------------
 
--- eemont/__init__.py
+++ eemont/__init__.py
@@ -1,6 +1,6 @@
 """The eemont python package."""
 
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 
 from . import (app, dataframe, eeDictionary, eeList, extra, feature,
                featurecollection, geometry, image, imagecollection, number)
